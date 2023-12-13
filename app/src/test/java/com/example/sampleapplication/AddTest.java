64376a39552a8544cd87676c67186b63b156ee28
ccddaa18 <cda86612@gmail.com>
2023-12-13 23:44:45 +0530

    test change

--- app/src/test/java/com/example/sampleapplication/AddTest.java
+++ app/src/test/java/com/example/sampleapplication/AddTest.java
@@ -6,6 +6,7 @@ public class AddTest extends TestCase{
 
     public void testCalc(){
         Add add=new Add();
+        Add add2=new Add();
         int result=add.calc(4,4);
         int expected=8;
         assertEquals(expected,result);
