088b910c070693a49e5805faf1c24db380b8fb9b
ccddaa18 <cda86612@gmail.com>
2023-12-14 10:53:31 +0530

    test commit

--- app/src/test/java/com/example/sampleapplication/AddTest.java
+++ app/src/test/java/com/example/sampleapplication/AddTest.java
@@ -6,7 +6,6 @@ public class AddTest extends TestCase{
 
     public void testCalc(){
         Add add=new Add();
-        Add add3=new Add();
         int result=add.calc(4,4);
         int expected=8;
         assertEquals(expected,result);
