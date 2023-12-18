9dc2bd615d9a259a75837226286afe994c2745f8
ccddaa18 <cda86612@gmail.com>
2023-12-18 12:10:30 +0530

    permission given to script file and errors inducted

--- app/src/test/java/com/example/sampleapplication/DivTest.java
+++ app/src/test/java/com/example/sampleapplication/DivTest.java
@@ -6,7 +6,7 @@ public class DivTest extends TestCase{
 
     public void testCalc(){
         Div div=new Div();
-        int result= div.calc(8,2);
+        int result=div.calc(8,2);
         int expected=4;
         assertEquals(expected,result);
     }
