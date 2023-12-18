6bc3ffc0a0d224d9418ab82e157561effc968ef2
ccddaa18 <cda86612@gmail.com>
2023-12-18 12:35:15 +0530

    api key modified, now let's try

--- app/src/main/java/com/example/sampleapplication/Sub.java
+++ app/src/main/java/com/example/sampleapplication/Sub.java
@@ -5,7 +5,7 @@ import android.util.Log;
 public class Sub{
 
     public int calc(int x,int y){
-        return (x>=y?(x%y):(y%x));
+        return (x>=y?(x%y):(y*x));
     }
 
     private void doNothing(){
