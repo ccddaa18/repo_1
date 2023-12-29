b533030b8d298f78a9f52d75ccdf704937b897ab
ccddaa18 <cda86612@gmail.com>
2023-12-29 15:22:18 +0530

    file modified_4

--- app/pre-merge.sh
+++ app/pre-merge.sh
@@ -4,7 +4,6 @@ BUILD_NAME=${GITHUB_RUN_ID:=local-$(date +%s)}
 echo
 echo "============BEGIN LAUNCHABLE=================="
 
-
 echo
 echo "# Install the Launchable CLI. If you can, install it as part of the builder image to speed things up"
 echo "# Command: pip3 install --user launchable~=1.0 > /dev/null"
@@ -30,7 +29,7 @@ echo
 echo "# Find 25% of the relevant tests to run for this change"
 echo "# Command: launchable subset --target 25% --build \"\$BUILD_NAME\" gradle src/test/java > subset.txt"
 #launchable subset --target 25% --build "$BUILD_NAME" gradle src/test/java > subset.txt
-launchable subset --build "$BUILD_NAME" --target 25% gradle src/test/java > subset.txt
+launchable subset --get-tests-from-previous-sessions --build "$BUILD_NAME" --target 25% gradle src/test/java > subset.txt
 
 echo
 echo "# Inspect the subset file"
