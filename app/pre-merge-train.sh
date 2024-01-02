2b00682c8d8da7d3b7b8842813b66d8da56b0030
ccddaa18 <cda86612@gmail.com>
2024-01-02 11:47:34 +0530

    different script written here

---
+++ app/pre-merge-train.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+BUILD_NAME=${GITHUB_RUN_ID:=local-$(date +%s)}
+
+# Get Launchable CLI installed. If you can, make it a part of the builder image to speed things up
+pip3 install --user launchable~=1.0 > /dev/null/
+export PATH=~/.local/bin:$PATH
+
+set -x
+# Verify that Launchable setup is all correct. Useful primarily while you work on integration
+launchable verify
+
+# Tell Launchable about the build you are producing and testing
+launchable record build --name "$BUILD_NAME" --source ..
+
+function record(){
+  # Record test results
+  launchable record test --build "$BUILD_NAME" gradle build/test-results/test
+}
+
+trap record EXIT
+
+./gradlew :app:test
