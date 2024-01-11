973386d916f286c5d373d81359d501949c523f02
ccddaa18 <cda86612@gmail.com>
2024-01-11 08:16:36 +0530

    file changed_6

--- gradle/pre-merge.sh
+++ gradle/pre-merge.sh
@@ -25,8 +25,10 @@ launchable record build --name "$BUILD_NAME" --source ..
 
 echo
 echo "# Find 25% of the relevant tests to run for this change"
-echo "# Command: launchable subset --target 25% --build \"\$BUILD_NAME\" gradle src/test/java > subset.txt"
+#echo "# Command: launchable subset --target 25% --build \"\$BUILD_NAME\" gradle src/test/java > subset.txt"
 #launchable subset --target 25% --build "$BUILD_NAME" gradle src/test/java > subset.txt
+
+echo "# Command: launchable subset --target 25% --build \"\$BUILD_NAME\" gradle src/test/java/com/example/sampleapplication > subset.txt"
 launchable subset --target 25% --build "$BUILD_NAME" gradle src/test/java/com/example/sampleapplication > subset.txt
 
 echo
